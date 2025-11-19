5497feecd4a7ebd05d0ffd33f910e4d75df40f02
Felicia Runfrost <Felicia.Runfrost@hotmail.com>
2025-11-19 13:47:19 +0100

    Integrate ASP.NET Core Identity for user management

--- TrainingTracker/Program.cs
+++ TrainingTracker/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Identity kopplad till vår DbContext
+// Identity kopplad till vï¿½r DbContext
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
