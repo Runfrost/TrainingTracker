e9f0e97a8ba76ac4a99472967fa23a0f7d7222d5
Felicia Runfrost <Felicia.Runfrost@hotmail.com>
2025-11-21 11:12:11 +0100

    Update connection string and add migration for user profile

--- TrainingTrackerAPI/Program.cs
+++ TrainingTrackerAPI/Program.cs
@@ -10,7 +10,9 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
