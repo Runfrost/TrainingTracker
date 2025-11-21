f685b0deecbdf2b609dbd5681885d6b5c9bf781b
David Rantanen <david.rantanen@telia.com>
2025-11-21 13:14:15 +0100

    added migrations

--- TrainingTrackerAPI/Controllers/ActivitiesController.cs
+++ TrainingTrackerAPI/Controllers/ActivitiesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TrainingTrackerAPI.Data;
+using TrainingTrackerAPI.DTO;
 using TrainingTrackerAPI.Models;
 
 namespace TrainingTrackerAPI.Controllers
@@ -28,6 +30,48 @@ namespace TrainingTrackerAPI.Controllers
             await _context.SaveChangesAsync();
 
             return Ok(running);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivityById(int id, ActivitiesEditDto editDto)
+        {
+            var activityToEdit = (Running)_context.Activities.Where(a => a.Id == id).SingleOrDefaultAsync().Result;
+            if(activityToEdit == null)
+            {
+                return NotFound();
+            }
+
+            activityToEdit.Name = editDto.Name;
+            activityToEdit.Distance = editDto.Distance;
+            activityToEdit.ActivityDate = editDto.ActivityDate;
+            activityToEdit.TotalTimeInSeconds = editDto.TotalTimeInSeconds;
+            activityToEdit.AverageCadence = editDto.AverageCadence;
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllActivities()
+        {
+            var activities = await _context.Activities.ToListAsync();
+            return Ok(activities);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetActivityById(int id)
+        {
+            var activity = await _context.Activities.FindAsync(id);
+            return Ok(activity);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivityById(int id)
+        {
+            var activityToDelete = await _context.Activities.FindAsync(id);
+            _context.Activities.Remove(activityToDelete);
+            await _context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
---
+++ TrainingTrackerAPI/DTO/ActivitiesEditDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainingTrackerAPI.DTO
+{
+    public class ActivitiesEditDto
+    {
+        public string Name { get; set; }
+        public double Distance { get; set; }
+        public DateTime ActivityDate { get; set; }
+        public int TotalTimeInSeconds { get; set; }
+        public int AverageCadence { get; set; }
+    }
+}
