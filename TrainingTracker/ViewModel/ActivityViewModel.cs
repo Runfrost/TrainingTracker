3c0f87af3614385dc5f06ae6c582d905161dd7b3
Glenn Wahlbeck <glenn.wahlbeck@campusnykoping.se>
2025-11-27 14:28:00 +0100

    removed some code from backend dto and created a dto for activities in the front end project

---
+++ TrainingTracker/ViewModel/ActivityViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainingTracker.ViewModel
+{
+    public class ActivityViewModel
+    {
+        public int? Id { get; set; }
+        [Required(ErrorMessage = "A name is required for the activity")]
+        [StringLength(20, ErrorMessage = "Name is too long, limit is 20")]
+        public string Name { get; set; }
+        [Required]
+        [Range(0.1, 250, ErrorMessage = "Activity too short or too long (max 250km)")]
+        public double Distance { get; set; }
+        [Required(ErrorMessage = "Type of activity needs to be selected")]
+        public string Type { get; set; }
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime ActivityDate { get; set; }
+
+        //public ApplicationUser User { get; set; }
+    }
+}
--- TrainingTrackerAPI/Models/Activity.cs
+++ TrainingTrackerAPI/Models/Activity.cs
@@ -5,13 +5,10 @@ namespace TrainingTrackerAPI.Models
     public class Activity
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "´Name is required")]
-        [StringLength(8, ErrorMessage = "Namnet är för långt")]
         public string Name { get; set; }
         public DateTime ActivityDate { get; set; }
         public int? AverageHeartRate { get; set; }
         public int? MaxHeartRate { get; set; }
-        [Range (0,2-200, ErrorMessage = "Du är dum i huvudet om du knappat in över 20 mil!")]
         public double Distance { get; set; }
         public int TotalTimeInSeconds { get; set; }
         public int? CaloriesBurned { get; set; }
