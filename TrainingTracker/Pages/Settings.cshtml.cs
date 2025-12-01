04fb7ca3e5651d2905846b00e88616ec64fec041
Felicia Runfrost <Felicia.Runfrost@hotmail.com>
2025-12-01 14:33:53 +0100

    Update user settings form and data retrieval logic
    - Changed form binding in `Settings.cshtml` to use `UserSettings.Name`.
    - Added input field for `UserSettings.Name` with a placeholder.
    - Introduced `UserSettingsDto` to manage user settings data.
    - Modified `OnGetAsync` in `Settings.cshtml.cs` to populate `UserSettings` with authenticated user details or set it to null if unauthenticated.

--- TrainingTracker/Pages/Settings.cshtml.cs
+++ TrainingTracker/Pages/Settings.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TrainingTracker.DAL;
+using TrainingTrackerAPI.DTO;
 using TrainingTrackerAPI.Models;
 
 namespace TrainingTracker.Pages
@@ -13,8 +14,25 @@ namespace TrainingTracker.Pages
         {
             _userManager = userManager;
         }
-        public void OnGetAsync()
+
+        public UserSettingsDto UserSettings { get; set; }
+        public async Task OnGetAsync()
         {
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                UserSettings = new UserSettingsDto
+                {
+                    Name = user?.Name,
+                    YearOfBirth = user?.YearOfBirth,
+                    Weight = user?.Weight,
+                    Height = user?.Height,
+                };
+            }
+            else
+            {
+                UserSettings = null;
+            }
         }
     }
 }
