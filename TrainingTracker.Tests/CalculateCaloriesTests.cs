7c1790fbad74085937045596bf1ef9f10914c58c
David Rantanen <david.rantanen@telia.com>
2025-12-03 10:47:49 +0100

    Changed test class

--- TrainingTracker.Tests/CalculateCaloriesTests.cs
+++ TrainingTracker.Tests/CalculateCaloriesTests.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainingTracker.Service;
 
 namespace TrainingTracker.Tests
 {
     public class CalculateCaloriesTests
     {
+        //private readonly CalorieService _calorieService;
+        //public CalculateCaloriesTests(CalorieService calorieService)
+        //{
+        //    _calorieService = calorieService;
+        //}
         [Fact]
         public void CalculateCalories_RunningActivity_ReturnsExpectedCalories()
         {
             // Arrange
-            var calorieService = new TrainingTracker.Service.CalorieService();
+            var calorieService = new CalorieService();
             double weight = 70; // in kg
             double duration = 3600; // in seconds
 
             var activity = TrainingTracker.FitConversion.FitSport.Running;
 
             // Act
-            double calories = calorieService.CalculateCalories(weight, duration, activity);
+            double calories = CalorieService.CalculateCalories(weight, duration, activity);
 
             // Assert
             double expectedCalories = 8.0 * weight * (duration / 3600.0); // MET for running is 8.0
