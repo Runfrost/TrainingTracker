cb35adc107d93a396f7d8f775fe842d17e50876c
Felicia Runfrost <Felicia.Runfrost@hotmail.com>
2025-12-03 13:50:08 +0100

    Updating Dashboard with statistic

--- TrainingTracker/Pages/Index.cshtml.cs
+++ TrainingTracker/Pages/Index.cshtml.cs
@@ -1,10 +1,11 @@
-using TrainingTrackerAPI.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using TrainingTracker.ViewModel;
 using TrainingTracker.DAL;
+using TrainingTracker.ViewModel;
+using TrainingTrackerAPI.Models;
+using static TrainingTracker.Pages.ActivitiesModel;
 
 namespace TrainingTracker.Pages
 {
@@ -29,6 +30,7 @@ namespace TrainingTracker.Pages
         public ViewModel.ActivityViewModel Activity { get; set; }
 
         public List<ActivityViewModel> Activities { get; set; }
+        public ActivityTotals ActivityTotal { get; set; } = new();
 
         public async Task OnGetAsync(int deleteId, int editId)
         {
@@ -101,6 +103,44 @@ namespace TrainingTracker.Pages
 
 
 
+        }
+
+        public class ActivityTotals
+        {
+            public double TotalDurationPreviousWeek { get; set; }
+            public double TotalDurationThisWeek { get; set; }
+            public double TotalDurationPreviousMonth { get; set; }
+            public double TotalDurationThisMonth { get; set; }
+            public double TotalCaloriesBurntPreviousWeek { get; set; }
+            public double TotalCaloriesBurntThisWeek { get; set; }
+            public double TotalCaloriesBurntPreviousMonth { get; set; }
+            public double TotalCaloriesBurntThisMonth { get; set; }
+            public int TotalActivitiesThisWeek { get; set; }
+            public int TotalActivitiesPreviousWeek { get; set; }
+            public int TotalActivitiesThisMonth { get; set; }
+            public int TotalActivitiesPreviousMonth { get; set; }
+            public double TotalDistanceThisWeek { get; set; }
+            public double TotalDistancePreviousWeek { get; set; }
+            public double TotalDistanceThisMonth { get; set; }
+            public double TotalDistancePreviousMonth { get; set; }
+
+            public List<MetricRow> WeeklyRows =>
+        new()
+        {
+            new MetricRow { Label = "Activities", ThisPeriod = TotalActivitiesThisWeek, PreviousPeriod = TotalActivitiesPreviousWeek },
+            new MetricRow { Label = "Distance", ThisPeriod = TotalDistanceThisWeek, PreviousPeriod = TotalDistancePreviousWeek, Unit = "km" },
+            new MetricRow { Label = "Duration", ThisPeriod = TotalDurationThisWeek, PreviousPeriod = TotalDurationPreviousWeek, Unit = "s" },
+            new MetricRow { Label = "Calories", ThisPeriod = TotalCaloriesBurntThisWeek, PreviousPeriod = TotalCaloriesBurntPreviousWeek, Unit = "cal" }
+        };
+
+            public List<MetricRow> MonthlyRows =>
+                new()
+                {
+            new MetricRow { Label = "Activities", ThisPeriod = TotalActivitiesThisMonth, PreviousPeriod = TotalActivitiesPreviousMonth },
+            new MetricRow { Label = "Distance", ThisPeriod = TotalDistanceThisMonth, PreviousPeriod = TotalDistancePreviousMonth, Unit = "km" },
+            new MetricRow { Label = "Duration", ThisPeriod = TotalDurationThisMonth, PreviousPeriod = TotalDurationPreviousMonth, Unit = "s" },
+            new MetricRow { Label = "Calories", ThisPeriod = TotalCaloriesBurntThisMonth, PreviousPeriod = TotalCaloriesBurntPreviousMonth, Unit = "cal" }
+                };
         }
     }
 }
