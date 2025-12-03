93cedcb35ed509a62ae9c8d757f34d4b778c9010
David Rantanen <david.rantanen@telia.com>
2025-12-03 15:11:57 +0100

    Added enum in backend

---
+++ TrainingTrackerAPI/Models/SportType.cs
@@ -0,0 +1,14 @@
+﻿namespace TrainingTrackerAPI.Models
+{
+    public enum SportType
+    {
+        Generic = 0,
+        Running = 1,
+        Cycling = 2,
+        Transition = 3,
+        FitnessEquipment = 4,
+        Swimming = 5,
+        Basketball = 6,
+        Walking = 7,
+    }
+}
